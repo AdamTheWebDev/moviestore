924bfe8c9676f80eb60de2caad2cc8b4fed91625
Adam Roberts <aroberts85@yahoo.com>
2017-06-01 13:04:49 -0400

    cart works

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'movies#index'
+
+  get 'carts/show'
+
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :movies, only: [:show, :index]
+  resource :cart, only: [:show] do
+    put 'add/:movie_id', to: 'carts#add', as: :add_to
+    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'movies#index'
 end
